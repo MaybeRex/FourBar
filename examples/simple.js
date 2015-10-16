1182fc71f1c6e06ef07ca613a4eeecd613fd484d
MaybeRex <marioxcore1@gmail.com>
2015-10-15 20:48:20 -0700

    thetaThree working

--- examples/simple.js
+++ examples/simple.js
@@ -1,4 +1,5 @@
 var FourBar = require('../lib/vectorFourBar.js');
 var four = new FourBar;
 
-console.log(four.outputAngle(  1.5  ,  4  ,  4.5  ,  5  ,  0.2  ));
+console.log('output angles' ,four.outputAngle(1.5, 4, 4.5, 5, 0.2));
+console.log('coupler angles' ,four.couplerAngle(1.5, 4, 4.5, 5, 0.2));
