7fcebfec896f76739199fb2415001983aab2fe84
MaybeRex <marioxcore1@gmail.com>
2015-10-16 21:40:54 -0700

    correctly returning type of FourBar

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,6 @@
 var FourBar = require('../lib/vectorFourBar.js');
 var four = new FourBar;
 
-console.log('output angles' ,four.outputAngle(1.5, 4, 4.5, 5, 0.2));
-console.log('coupler angles' ,four.couplerAngle(1.5, 4, 4.5, 5, 0.2));
+// console.log('output angles' ,four.outputAngle(1.5, 4, 4.5, 5, 0.2));
+// console.log('coupler angles' ,four.couplerAngle(1.5, 4, 4.5, 5, 0.2));
+console.log(four.linkageType(1,1.5,1,1.5));
