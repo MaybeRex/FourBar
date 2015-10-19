e9664de32f2ac6d10de3fe500417b2ebbd3b6406
MaybeRex <marioxcore1@gmail.com>
2015-10-18 22:53:45 -0700

    hotfix

--- examples/simple.js
+++ examples/simple.js
@@ -5,9 +5,15 @@ var four = new FourBar;
 var output;
 var coupler;
 
+var link1 = 8;
+var link2 = 8;
+var link3 = 8;
+var link4 = 8;
+angle = (75 * (Math.PI/180));
 
-output = four.outputAngle(2, 7, 9, 6, 0.523599);
-coupler = four.couplerAngle(2, 7, 9, 6, 0.523599);
+
+output = four.outputAngle(link2, link3, link4, link1, angle);
+coupler = four.couplerAngle(link2, link3, link4, link1, angle);
 
 console.log('');
 console.log('crossed output angle', (output.crossed * (180/Math.PI)));
@@ -19,4 +25,4 @@ console.log('crossed coupler angle', (coupler.crossed * (180/Math.PI)));
 console.log('');
 console.log('open coupler angle', (coupler.open * (180/Math.PI)));
 console.log('');
-console.log('TYPE: ', four.linkageType(2, 7, 9, 6, 0.523599));
+console.log('TYPE: ', four.linkageType(link2, link3, link4, link1, angle));
