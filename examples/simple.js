7cf29885db87232ca4e68cf35a0e29d4a90064d6
MaybeRex <marioxcore1@gmail.com>
2015-10-18 20:10:50 -0700

    added a comment to the example

--- examples/simple.js
+++ examples/simple.js
@@ -1,6 +1,8 @@
 var FourBar = require('../lib/vectorFourBar.js');
 var four = new FourBar;
 
-// console.log('output angles' ,four.outputAngle(1.5, 4, 4.5, 5, 0.2));
-//console.log('coupler angles' ,four.outputAngle(1,1.5,1,1.5,0.5));
-console.log(four.outputVector(1,1.5,1,1.5,0.5));
+//This example quickly logs information about a fourbar at angle 0.2 radians
+
+console.log('output angle' ,four.outputAngle(1.5, 4, 4.5, 5, 0.2));
+console.log('coupler angles' ,four.outputAngle(1,1.5,1,1.5,0.5));
+console.log('outputVector', four.outputVector(1,1.5,1,1.5,0.5));
