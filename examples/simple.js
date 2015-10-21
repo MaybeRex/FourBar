8618696d0935f5cc8f14f1643c8d12f9b1a5fde4
MaybeRex <marioxcore1@gmail.com>
2015-10-21 11:54:56 -0700

    adding transmission angle fuctionality

--- examples/simple.js
+++ examples/simple.js
@@ -4,25 +4,31 @@ var four = new FourBar;
 
 var output;
 var coupler;
+var transmission;
 
-var link1 = 8;
-var link2 = 8;
-var link3 = 8;
-var link4 = 8;
+var link1 = 20;
+var link2 = 10;
+var link3 = 10;
+var link4 = 10;
 angle = (75 * (Math.PI/180));
 
 
 output = four.outputAngle(link2, link3, link4, link1, angle);
 coupler = four.couplerAngle(link2, link3, link4, link1, angle);
+transmission = four.transmissionAngle(link2, link3, link4, link1, angle);
 
 console.log('');
 console.log('crossed output angle', (output.crossed * (180/Math.PI)));
 console.log('');
 console.log('open output angle', (output.open * (180/Math.PI)));
 console.log('');
-
 console.log('crossed coupler angle', (coupler.crossed * (180/Math.PI)));
 console.log('');
 console.log('open coupler angle', (coupler.open * (180/Math.PI)));
 console.log('');
 console.log('TYPE: ', four.linkageType(link2, link3, link4, link1, angle));
+console.log('');
+console.log('');
+console.log('crossed transmission angle', (transmission.crossed * (180/Math.PI)));
+console.log('');
+console.log('open transmission angle', (transmission.open * (180/Math.PI)));
